015d9dcf5e78f074e05ca21f019ce58cea4afc01
redVi <liberty195@gmail.com>
2018-11-26 23:26:11 +0700

    TodoCount test

---
+++ tests/unit/todo-count.spec.ts
@@ -0,0 +1,18 @@
+import TodoCount from '@/components/TodoCount.vue';
+import { shallowMount } from '@vue/test-utils';
+
+describe('TodoCount.vue', () => {
+  const wrapper = shallowMount(TodoCount, { propsData: { list: 2 } });
+
+  it('correctly declines words', () => {
+    // exact text match
+    expect( wrapper.find('strong').text() ).toBe('2 items left');
+
+    // approximate text match
+    wrapper.setProps({ list: 1 });
+    expect( wrapper.find('strong').text() ).toContain('item left');
+
+    wrapper.setProps({ list: 378 });
+    expect( wrapper.find('strong').text() ).toContain('items left');
+  });
+});
